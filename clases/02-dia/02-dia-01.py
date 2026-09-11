89b72e532146785342b442138f1572bb68d41cf7
jpruizl <tu-email@ejemplo.com>
2026-09-11 08:08:02 -0600

    update día 2

---
+++ clases/02-dia/02-dia-01.py
@@ -0,0 +1,70 @@
+print('Operadores Abreviados')
+print('se les llama Short Code')
+
+x = 5
+dato = 3
+
+x = x * 2
+dato = dato + 1
+
+print(x)
+print(dato)
+
+x = 5
+dato = 3
+
+x *= 2
+dato += 1
+
+print(x, dato)
+
+print()
+print()
+
+i = 2
+j = 2
+
+i = i + 2 * j
+print(i)
+
+i = 2
+j = 2
+i += 2 * j
+print(i)
+
+print()
+print()
+
+var = 10    
+var = var / 2
+print(var)
+
+print()
+
+var = 10
+var /= 2
+print(var)
+
+print()
+
+rem = 5
+rem = rem % 10
+print(rem)
+
+print()
+
+rem = 5
+rem %= 10
+print(rem)
+
+print()
+
+x = 3
+x = x ** 2
+print(x)
+
+print()
+
+x = 3
+x **= 2
+print(x)
---
+++ clases/02-dia/02-dia-02.py
@@ -0,0 +1,23 @@
+# Autor: Juan Pablo Ruiz
+# Fecha: 11/09/2026
+# Versión: 1.0.0
+
+print('Aprendiendo de comentarios')
+
+num1 = 100
+num2 = 200
+
+# proceso donde realizamos la suma
+print('La suma es:', num1 + num2)
+
+saldo = num1 * num2  # variable que almacena el saldo
+
+#print('Muy útil para cerrar líneas')
+
+
+""" Este es un comentario de varias líneas
+muy útiles para determinar o explicar un bloque
+de código que requiera esa descripción """
+
+print('Aquí ya comienza mi programa')
+
